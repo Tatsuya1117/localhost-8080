c0b5ac35b696a393415f969c8820c9d4a02e005f
TatsuyaOzawa <othe.world.pc@gmail.com>
2019-10-13 17:07:44 +0900

    修正：(詳細、変更、削除の位置)

--- index.php
+++ index.php
@@ -24,12 +24,12 @@ try {
 	echo "<tr>\n";
 		echo "<td>" .htmlspecialchars($row['recipe_name'],ENT_QUOTES,'UTF-8'). "</td>\n";
 		echo "<td>" .htmlspecialchars($row['budget'],ENT_QUOTES,'UTF-8'). "</td>\n";
+		echo "<td>" .htmlspecialchars($row['difficulty'],ENT_QUOTES,'UTF-8'). "</td>\n";
 		echo "<td>\n";
 		echo "<a href=detail.php?id=" . htmlspecialchars($row['id'], ENT_QUOTES, 'UTF-8') . ">詳細</a>\n";
 		echo "|<a href=edit.php?id=" .htmlspecialchars($row['id'], ENT_QUOTES, 'UTF-8') .">変更</a>\n";
 		echo "|<a href=delete.php?id=" .htmlspecialchars($row['id'], ENT_QUOTES, 'UTF-8') . ">削除</a>\n";
 		echo "</td>\n";
-		echo "<td>" .htmlspecialchars($row['difficulty'],ENT_QUOTES,'UTF-8'). "</td>\n";
 		echo "</tr>\n";
 	}
 	echo "</table>\n";
